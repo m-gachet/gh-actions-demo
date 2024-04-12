c4689ca10939d470329aec625d6628e81e6bf1f2
Marcelo Gachet <marcelo@growdnd.com>
2024-04-11 20:48:25 -0700

    changed the variable name

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-const App = 12
+const testVariable = 12;
 function App() {
   const [count, setCount] = useState(0)
 
